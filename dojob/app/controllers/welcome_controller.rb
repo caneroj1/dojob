a5dce8b9bb91879c77476da53728db03f20c7631
caneroj1 <caneroj1@tcnj.edu>
2014-10-31 14:51:56 -0400

    Changes to Log In and Sign Up, Forgot Pass, Colors

--- dojob/app/controllers/welcome_controller.rb
+++ dojob/app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    enable_devise
   end
 end
--- dojob/app/models/user.rb
+++ dojob/app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :email, :state, :city, :zip, :first_name, :last_name, :password, :address
+
+  # VALIDATIONS
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true, format: { with: /\A[0-9]\z/, message: "allows only numbers" }, length: { is: 5 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
 end
