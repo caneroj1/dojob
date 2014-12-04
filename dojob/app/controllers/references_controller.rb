362a948038fa973bd7f0659f4445f8c47c684e3c
caneroj1 <caneroj1@tcnj.edu>
2014-12-04 02:12:10 -0500

    added functionality to add new references on the edit profile page. need to fix numbering when deleting and adding a new ref after deleting.

--- dojob/app/controllers/references_controller.rb
+++ dojob/app/controllers/references_controller.rb
@@ -8,6 +8,22 @@ class ReferencesController < ApplicationController
     end
   end
 
+  def create
+    reference_hash = { first_name:     params[:first_name],
+                       last_name:      params[:last_name],
+                       contact_email:  params[:contact_email],
+                       contact_number: params[:contact_number] }
+
+    reference = Reference.create(reference_hash)
+    current_user.references << reference
+    
+    params[:id] = reference.id
+
+    respond_to do |format|
+      format.js { render layout: false }
+    end
+  end
+
   def destroy
     reference = Reference.find_by_id(params[:id])
     reference.delete
