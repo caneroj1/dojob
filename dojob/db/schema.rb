e44f4ad3491be0204e9790857b101bc3124f92c9
caneroj1 <caneroj1@tcnj.edu>
2014-12-26 14:43:11 -0500

    migrated to postgres.

--- dojob/db/schema.rb
+++ dojob/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141222024702) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "offer_id"
@@ -101,7 +104,7 @@ ActiveRecord::Schema.define(version: 20141222024702) do
     t.string   "avatar"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
