98087943d610a6a7fa0ccfe3db1b24dd04bfba9e
caneroj1 <caneroj1@tcnj.edu>
2015-01-17 11:42:27 -0500

    more tests for the user model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,13 @@ FactoryGirl.define do
     f.last_name   { Faker::Name.last_name }
     f.state       { Faker::Address.state }
     f.city        { Faker::Address.city }
-    f.zip         { Faker::Address.zip_code }
+    f.zip         { zip = ""; 5.times { zip  << rand(9).to_s }; zip; }
     f.address     { Faker::Address.street_name }
     f.email       { Faker::Internet.email }
     f.password    { Faker::Internet.password(10) }
+
+    transient do
+      invalid nil
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,91 @@
 
 describe User do
+  let(:user) { FactoryGirl.create(:user) }
+  words_regexp = /([A-z][a-z]\s*)+/
+  email_regexp = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+
   it "has a valid factory" do
-    expect(FactoryGirl.create(:user)).to be_valid
+    expect(user).to be_valid
   end
 
-  it "has an email"
-  it "has a state"
-  it "has a city"
-  it "has a zip"
-  it "has a first name"
-  it "has a last name"
-  it "has a password"
-  it "has an address"
-  it "can be paid"
-  it "is invalid without email"
-  it "is invalid without state"
-  it "is invalid without city"
-  it "is invalid without zip"
-  it "is invalid without first name"
-  it "is invalid without last name"
-  it "is invalid without address"
+  context "attributes" do
+    it "has an email" do
+     expect(user.email).to match(email_regexp)
+   end
+
+    it "has a state" do
+      expect(user.state).to match(words_regexp)
+    end
+
+    it "has a city" do
+      expect(user.city).to match(words_regexp)
+    end
+
+    it "has a zip" do
+      expect(user.zip).to match(/[0-9]{5}/)
+    end
+
+    it "has a first name" do
+      expect(user.first_name).to match(words_regexp)
+    end
+
+    it "has a last name" do
+      expect(user.last_name).to match(words_regexp)
+    end
+
+    it "has a password" do
+      expect(user.password).to match(/(\w|[~`@#\$\%\^\&\*\(\)\_\-\=\+\{\}\[\]\|\/\\]){8,}/)
+    end
+
+    it "has an address" do
+      expect(user.address).to match(words_regexp)
+    end
+
+    it "starts as a free user" do
+      expect(user.paid).to eq false
+    end
+
+    it "is invalid without email" do
+      expect(FactoryGirl.build(:user, email: nil)).to_not be_valid
+    end
+
+    it "is invalid without state" do
+      expect(FactoryGirl.build(:user, state: nil)).to_not be_valid
+    end
+
+    it "is invalid without city" do
+      expect(FactoryGirl.build(:user, city: nil)).to_not be_valid
+    end
+
+    it "is invalid without zip" do
+      expect(FactoryGirl.build(:user, zip: nil)).to_not be_valid
+    end
+
+    it "is invalid without first name" do
+      expect(FactoryGirl.build(:user, first_name: nil)).to_not be_valid
+    end
+
+    it "is invalid without last name" do
+      expect(FactoryGirl.build(:user, last_name: nil)).to_not be_valid
+    end
+
+    it "is invalid without address" do
+      expect(FactoryGirl.build(:user, address: nil)).to_not be_valid
+    end
+
+    it "is invalid without password" do
+      expect(FactoryGirl.build(:user, password: nil)).to_not be_valid
+    end
+
+    it "expects email to be unique" do
+      user_one = FactoryGirl.create(:user, email: "testemail@mail.com")
+      expect(FactoryGirl.build(:user, email: "testemail@mail.com")).to_not be_valid
+    end
+  end
+
+  context "instance methods" do
+    it "#my_name returns the correctly formatted name string" do
+      expect(user.my_name).to eq("#{user.first_name} #{user.last_name}")
+    end
+  end
 end
